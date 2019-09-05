28c581bc01ef8de42c910287ef8f8b8cb7fbe0ad
Jon Caneiro <joncaneiro@gmail.com>
2019-09-05 02:13:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,7 +14,7 @@ end
 
 def introduction_with_language(name, language)
   
-  puts "Hi, my name is #{name} and I am learning #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
 end
 
@@ -22,7 +22,7 @@ end
 
 def introduction_with_language_optional(name, language = 'Ruby')
   
-  puts "Hi, my name is #{name} and I am learning #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
 end
 
